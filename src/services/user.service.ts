7c8d15cd3202e200b1625196deb50347276487e7
prashams <shahprashamb@gmail.com>
2026-01-02 14:51:26 +0530

    users get api changes

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import User from '../models/user.model';
 import UserRole from '../models/user_role.model';
 import { encryptPassword } from '../utils/password.util';
+import { Menu, Role } from 'src/models';
 
 @Injectable()
 export default class UserService {
@@ -34,9 +35,40 @@ export default class UserService {
    * Get all users
    */
   async findAll() {
-    return User.findAll({
+    const users = await User.findAll({
       attributes: { exclude: ['password'] },
+
+      include: [
+        {
+          model: Role,
+          as: 'roles',
+          attributes: ['role_id', 'name'],
+
+          through: { attributes: [] },
+
+          include: [
+            {
+              model: Menu,
+              as: 'menus',
+              attributes: [
+                'menu_id',
+                'menu_group_id',
+                'name',
+                'path',
+                'icon',
+                'order',
+                'status',
+              ],
+              through: { attributes: [] },
+            },
+          ],
+        },
+      ],
+
+      subQuery: false,
     });
+
+    return this.flattenUserRolesAndMenus(users);
   }
 
   /**
@@ -71,4 +103,45 @@ export default class UserService {
 
     return { success: true, userId };
   }
+
+  private flattenUserRolesAndMenus(users: any[]) {
+    return users.map((user) => {
+      const roleMap = new Map<number, any>();
+      const menuMap = new Map<number, any>();
+
+      user.roles?.forEach((role) => {
+        // collect unique roles
+        roleMap.set(role.role_id, {
+          role_id: role.role_id,
+          name: role.name,
+        });
+
+        // collect unique menus across all roles
+        role.menus?.forEach((menu) => {
+          menuMap.set(menu.menu_id, {
+            menu_id: menu.menu_id,
+            menu_group_id: menu.menu_group_id,
+            name: menu.name,
+            path: menu.path,
+            icon: menu.icon,
+            order: menu.order,
+            status: menu.status,
+          });
+        });
+      });
+
+      return {
+        user_id: user.user_id,
+        full_name: user.full_name,
+        username: user.username,
+        user_type_id: user.user_type_id,
+        createdAt:user.createdAt,
+        updatedAt:user.updatedAt,
+
+        roles: Array.from(roleMap.values()),
+        menus: Array.from(menuMap.values()),
+      };
+    });
+  }
+
 }
